c5427451be937bbcba9eddfd6a9ca4f125cd4e49
Salvatore <dipietro.salvatore@gmail.com>
2019-11-29 14:11:04 +0000

    update for terraform v0.12

--- main.tf
+++ main.tf
@@ -5,12 +5,12 @@ resource "aws_inspector_resource_group" "myinstances" {
 
 resource "aws_inspector_assessment_target" "myinspect" {
   name = var.inspector_assessment_target_name
-  resource_group_arn = "${aws_inspector_resource_group.myinstances.arn}"
+  resource_group_arn = aws_inspector_resource_group.myinstances.arn
 }
 
 resource "aws_inspector_assessment_template" "mytemplate" {
   name       = var.inspector_assessment_template_name
-  target_arn = "${aws_inspector_assessment_target.myinspect.arn}"
+  target_arn = aws_inspector_assessment_target.myinspect.arn
   duration   = var.inspector_assessment_template_duration
 
   rules_package_arns = var.inspector_rules_packages
