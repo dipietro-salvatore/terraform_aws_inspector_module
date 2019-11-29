13694f179d36d22a57a1f1da2031fd1bfca3da60
Salvatore <dipietro.salvatore@gmail.com>
2019-11-29 15:12:56 +0000

    added count to enable create objects or not

--- main.tf
+++ main.tf
@@ -1,14 +1,20 @@
 
 resource "aws_inspector_resource_group" "myinstances" {
+  count = var.create ? 1 : 0
+  
   tags =  var.instances_tags
 }
 
 resource "aws_inspector_assessment_target" "myinspect" {
+  count = var.create ? 1 : 0
+  
   name = var.inspector_assessment_target_name
   resource_group_arn = aws_inspector_resource_group.myinstances.arn
 }
 
 resource "aws_inspector_assessment_template" "mytemplate" {
+  count = var.create ? 1 : 0
+  
   name       = var.inspector_assessment_template_name
   target_arn = aws_inspector_assessment_target.myinspect.arn
   duration   = var.inspector_assessment_template_duration
