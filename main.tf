2774f7480c01aa4f00a410fcb359b98338ef0141
Salvatore <dipietro.salvatore@gmail.com>
2019-11-29 15:20:18 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,14 +9,14 @@ resource "aws_inspector_assessment_target" "myinspect" {
   count = var.create ? 1 : 0
   
   name = var.inspector_assessment_target_name
-  resource_group_arn = aws_inspector_resource_group.myinstances.arn
+  resource_group_arn = aws_inspector_resource_group.myinstances[0].arn
 }
 
 resource "aws_inspector_assessment_template" "mytemplate" {
   count = var.create ? 1 : 0
   
   name       = var.inspector_assessment_template_name
-  target_arn = aws_inspector_assessment_target.myinspect.arn
+  target_arn = aws_inspector_assessment_target.myinspect[0].arn
   duration   = var.inspector_assessment_template_duration
 
   rules_package_arns = var.inspector_rules_packages
